70b1628d2c0e81d399107a113051855055b3213a
adampridmore <adam_pridmore@hotmail.com>
2021-07-29 20:59:40 +0100

    Plots fn of x

--- ColourGraph.fsx
+++ ColourGraph.fsx
@@ -2,41 +2,43 @@
 
 open XPlot.Plotly
 open System.Numerics
+open System
 
+let PI = Math.PI
 
 let toPlotPoints (cseq: seq<Complex>) = 
-  cseq |> Seq.map (fun x -> x.Real, x.Imaginary )
+  cseq 
+  |> Seq.map (fun x -> x.Real, x.Imaginary )
+  |> Seq.toList
+  |> List.unzip
 
-let s = [
-  Complex(1.0, 2.0);
-  Complex(2.0, 1.0)
-]
+let s =
+  Complex(1.0, 0.0)
+  |> Seq.unfold(fun c -> Some(c, c * Complex(Math.Cos(PI / 4.0), Math.Sin( PI / 4.0)) ))
+  |> Seq.take 10
 
 let fn s = s + Complex(1.0,0.0)
 
-let points1 = ([
-                  Complex(1.0, 2.0);
-                  Complex(2.0, 1.0)
-              ])
-              |> toPlotPoints |> Seq.toList |> List.unzip
+// let graphFns (fn : Complex -> Complex) (s : seq<Complex>) : Unit =
+//   ()
 
-// let points2 = [1.0;2.0], [3.0;4.0]
+let s2 = s |> Seq.map fn
 
-let points2 = 
-  s |> Seq.map fn
-  |> toPlotPoints |> Seq.toList |> List.unzip
-
+let points1 = s |> toPlotPoints 
+let points2 = s2 |> toPlotPoints
 
 let scatter1 =
   Scatter(
       x = (points1 |> fst),
-      y = (points1 |> snd)
+      y = (points1 |> snd),
+      name = "s"
   )
 
 let scatter2 =
   Scatter(
       x = (points2 |> fst),
-      y = (points2 |> snd)
+      y = (points2 |> snd),
+      name = "fn(s)"
   )
 
 [scatter1;scatter2] |> Chart.Plot |> Chart.Show
