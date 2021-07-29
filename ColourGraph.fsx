34d7047372b3ea693ec466229f89a3f3eea1721a
adampridmore <adam_pridmore@hotmail.com>
2021-07-29 21:18:05 +0100

    Function to plot s and fn(s)

--- ColourGraph.fsx
+++ ColourGraph.fsx
@@ -4,41 +4,44 @@ open XPlot.Plotly
 open System.Numerics
 open System
 
+let graphFns (fn : Complex -> Complex) (s : seq<Complex>) : Unit =
+
+  let toPlotPoints (cseq: seq<Complex>) = 
+    cseq 
+    |> Seq.map (fun x -> x.Real, x.Imaginary )
+    |> Seq.toList
+    |> List.unzip
+
+  let s2 = s |> Seq.map fn
+
+  let points1 = s |> toPlotPoints 
+  let points2 = s2 |> toPlotPoints
+
+  let scatter1 =
+    Scatter(
+        x = (points1 |> fst),
+        y = (points1 |> snd),
+        name = "s"
+    )
+
+  let scatter2 =
+    Scatter(
+        x = (points2 |> fst),
+        y = (points2 |> snd),
+        name = "fn(s)"
+    )
+
+  [scatter1;scatter2] |> Chart.Plot |> Chart.Show
+
+  ()
+
 let PI = Math.PI
 
-let toPlotPoints (cseq: seq<Complex>) = 
-  cseq 
-  |> Seq.map (fun x -> x.Real, x.Imaginary )
-  |> Seq.toList
-  |> List.unzip
-
 let s =
-  Complex(1.0, 0.0)
-  |> Seq.unfold(fun c -> Some(c, c * Complex(Math.Cos(PI / 4.0), Math.Sin( PI / 4.0)) ))
-  |> Seq.take 10
+  Complex(1.5, 0.0)
+  |> Seq.unfold(fun c -> Some(c, c * Complex(Math.Cos(PI / 32.0), Math.Sin( PI / 32.0)) ))
+  |> Seq.take 65
 
-let fn s = s + Complex(1.0,0.0)
+let fn (s : Complex) : Complex = s ** s
 
-// let graphFns (fn : Complex -> Complex) (s : seq<Complex>) : Unit =
-//   ()
-
-let s2 = s |> Seq.map fn
-
-let points1 = s |> toPlotPoints 
-let points2 = s2 |> toPlotPoints
-
-let scatter1 =
-  Scatter(
-      x = (points1 |> fst),
-      y = (points1 |> snd),
-      name = "s"
-  )
-
-let scatter2 =
-  Scatter(
-      x = (points2 |> fst),
-      y = (points2 |> snd),
-      name = "fn(s)"
-  )
-
-[scatter1;scatter2] |> Chart.Plot |> Chart.Show
+graphFns fn s
