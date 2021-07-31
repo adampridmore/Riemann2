dd1593ab8a53d19f72a49c5841e8bac6571fb8dd
Adam Pridmore <adam_pridmore@hotmail.com>
2021-07-31 08:10:07 +0100

    Add seq unzip

--- modules/Helpers.fsx
+++ modules/Helpers.fsx
@@ -10,3 +10,12 @@ let PI = Math.PI
 // Complex(0.0, 0.0)
 // |> initInfinite increment
 // |> Seq.take 4
+
+
+let unzip (s: seq<'T1 * 'T2>) =
+  let it1, it2 = 
+      s 
+      |> List.ofSeq 
+      |> List.unzip 
+  (Seq.ofList it1, Seq.ofList it2)
+  
